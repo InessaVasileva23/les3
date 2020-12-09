91bb748c5ee9981d3117a874ac3085816f35c283
Inessa <i.vasileva.stc@innopolis.university.ru>
2020-12-09 11:11:50 +0300

    домашнее задание

---
+++ src/com/task1/Main.java
@@ -0,0 +1,14 @@
+package com.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+            double price = 42.50;
+            int volume = 50;
+
+            double money = price * volume;
+            System.out.println("За бензин нужно заплатить: " + money + " рублей") ;
+
+        }
+
+    }
